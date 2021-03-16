79aed32199ad90690f822f3d382221e7fb001201
Richard Robles <rrobles9112@gmail.com>
2021-03-16 11:16:34 -0500

    Initial commit

---
+++ Main.py
@@ -0,0 +1,19 @@
+class LetterFilter(object):
+
+    def __init__(self, s):
+        self.s = s
+
+    def filter_vowels(self):
+        return "".join([char for char in self.s if char in "aeiouAEIOU"])
+
+    def filter_consonants(self):
+        return "".join([char for char in self.s if char not in "aeiouAEIOU"])
+
+
+# Enter your code here
+# Return a string
+s = input()
+f = LetterFilter(s)
+
+print(f.filter_vowels())
+print(f.filter_consonants())
